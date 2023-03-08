d39cfbe0c408024aedf6478b9cc58c395428d7e9
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-03-08 16:35:31 +0900

    명령어 입력

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -8,11 +8,14 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		String command = sc.nextLine();
-		System.out.printf("입력된 명령어: %s\n", command);
-		
-		int command2 = sc.nextInt();
-		System.out.printf("입력된 명령어: %d\n", command2);
+		while(true) {
+			System.out.print("명령어 >> ");
+			String command = sc.nextLine();
+			
+			if (command.equals("exit")) {
+				break;
+			}
+		}
 		
 		System.out.println("==프로그램 끝==");
 		
@@ -20,4 +23,4 @@ public class Main {
 	}
 }
 //브라우저, DB 없이 명령어를 입력하는 방식으로 콘솔에서 운영되는 게시판이라고 가정
-// 1. 게시글 관련 기능, 2. 회원 관련 기능
+// 1. 게시글 관련 기능(Article 혹은 Post), 2. 회원 관련 기능(Member)
