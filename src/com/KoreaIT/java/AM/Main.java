13047061917988758dd90a948997230b4f7cf37d
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-03-08 15:58:03 +0900

    명령어 입력 테스트

---
+++ src/com/KoreaIT/java/AM/Main.java
@@ -0,0 +1,23 @@
+package com.KoreaIT.java.AM;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String command = sc.nextLine();
+		System.out.printf("입력된 명령어: %s\n", command);
+		
+		int command2 = sc.nextInt();
+		System.out.printf("입력된 명령어: %d\n", command2);
+		
+		System.out.println("==프로그램 끝==");
+		
+		sc.close(); // 자원 사용 종료를 위해 원칙적으로는 꺼줘야함
+	}
+}
+//브라우저, DB 없이 명령어를 입력하는 방식으로 콘솔에서 운영되는 게시판이라고 가정
+// 1. 게시글 관련 기능, 2. 회원 관련 기능
